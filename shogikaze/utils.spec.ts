376d4097c24f599911550bd5a0ccce5c8bbc6196
d-issy <12374694+d-issy@users.noreply.github.com>
2022-07-15 22:00:01 +0900

    fmt

--- shogikaze/utils.spec.ts
+++ shogikaze/utils.spec.ts
@@ -1,68 +1,68 @@
-import { COLOR, Color, FILE, File, Move, Piece, PIECE, RANK, Rank, ROLE, Role, SQUARE, Square } from "./types"
+import { COLOR, Color, FILE, File, Hand, Move, Piece, PIECE, RANK, Rank, ROLE, Role, SQUARE, Square } from "./types"
 import * as utils from "./utils"
 
 describe("toggleColor", () => {
-  const tests: Array<{color: Color; want: Color}> = [
-    {color: COLOR.Black, want: COLOR.White},
-    {color: COLOR.White, want: COLOR.Black},
+  const tests: Array<{ color: Color; want: Color }> = [
+    { color: COLOR.Black, want: COLOR.White },
+    { color: COLOR.White, want: COLOR.Black },
   ]
-  test.each(tests)("toggleColor($color) should be $want", ({color, want}) => {
+  test.each(tests)("toggleColor($color) should be $want", ({ color, want }) => {
     expect(utils.toggleColor(color)).toBe(want)
   })
 })
 
 describe("togglePiece", () => {
-  const tests: Array<{piece: Piece; want: Piece}> = [
-    {piece: PIECE.BPawn, want: PIECE.WPawn},
-    {piece: PIECE.WPawn, want: PIECE.BPawn},
-    {piece: PIECE.BKing, want: PIECE.WKing},
-    {piece: PIECE.WKing, want: PIECE.BKing},
-    {piece: PIECE.BDragon, want: PIECE.WDragon},
-    {piece: PIECE.WDragon, want: PIECE.BDragon},
+  const tests: Array<{ piece: Piece; want: Piece }> = [
+    { piece: PIECE.BPawn, want: PIECE.WPawn },
+    { piece: PIECE.WPawn, want: PIECE.BPawn },
+    { piece: PIECE.BKing, want: PIECE.WKing },
+    { piece: PIECE.WKing, want: PIECE.BKing },
+    { piece: PIECE.BDragon, want: PIECE.WDragon },
+    { piece: PIECE.WDragon, want: PIECE.BDragon },
   ]
-  test.each(tests)("togglePiece($piece) should be $want", ({piece, want}) => {
+  test.each(tests)("togglePiece($piece) should be $want", ({ piece, want }) => {
     expect(utils.togglePiece(piece)).toBe(want)
   })
 })
 
 describe("colorOf", () => {
-  const tests: Array<{piece: Piece; want: Color}> = [
-    {piece: PIECE.BPawn, want: COLOR.Black},
-    {piece: PIECE.WPawn, want: COLOR.White},
-    {piece: PIECE.BKing, want: COLOR.Black},
-    {piece: PIECE.WKing, want: COLOR.White},
-    {piece: PIECE.BDragon, want: COLOR.Black},
-    {piece: PIECE.WProPawn, want: COLOR.White},
+  const tests: Array<{ piece: Piece; want: Color }> = [
+    { piece: PIECE.BPawn, want: COLOR.Black },
+    { piece: PIECE.WPawn, want: COLOR.White },
+    { piece: PIECE.BKing, want: COLOR.Black },
+    { piece: PIECE.WKing, want: COLOR.White },
+    { piece: PIECE.BDragon, want: COLOR.Black },
+    { piece: PIECE.WProPawn, want: COLOR.White },
   ]
-  test.each(tests)("colorOf($piece) should be $want", ({piece, want}) => {
+  test.each(tests)("colorOf($piece) should be $want", ({ piece, want }) => {
     expect(utils.colorOf(piece)).toBe(want)
   })
 })
 
 describe("roleOf", () => {
-  const tests: Array<{piece: Piece; want: Role}> = [
-    {piece: PIECE.BPawn, want: ROLE.Pawn},
-    {piece: PIECE.WPawn, want: ROLE.Pawn},
-    {piece: PIECE.BKing, want: ROLE.King},
-    {piece: PIECE.WKing, want: ROLE.King},
-    {piece: PIECE.BDragon, want: ROLE.Dragon},
-    {piece: PIECE.WProPawn, want: ROLE.ProPawn},
+  const tests: Array<{ piece: Piece; want: Role }> = [
+    { piece: PIECE.BPawn, want: ROLE.Pawn },
+    { piece: PIECE.WPawn, want: ROLE.Pawn },
+    { piece: PIECE.BKing, want: ROLE.King },
+    { piece: PIECE.WKing, want: ROLE.King },
+    { piece: PIECE.BDragon, want: ROLE.Dragon },
+    { piece: PIECE.WProPawn, want: ROLE.ProPawn },
   ]
-  test.each(tests)("roleOf($piece) should be $want", ({piece, want}) => {
+  test.each(tests)("roleOf($piece) should be $want", ({ piece, want }) => {
     expect(utils.roleOf(piece)).toBe(want)
   })
 })
 
 describe("promotionOf", () => {
-  const tests: Array<{piece: Piece; want: boolean}> = [
-    {piece: PIECE.BPawn, want: false},
-    {piece: PIECE.WPawn, want: false},
-    {piece: PIECE.BKing, want: false},
-    {piece: PIECE.WKing, want: false},
-    {piece: PIECE.BDragon, want: true},
-    {piece: PIECE.WProPawn, want: true},
+  const tests: Array<{ piece: Piece; want: boolean }> = [
+    { piece: PIECE.BPawn, want: false },
+    { piece: PIECE.WPawn, want: false },
+    { piece: PIECE.BKing, want: false },
+    { piece: PIECE.WKing, want: false },
+    { piece: PIECE.BDragon, want: true },
+    { piece: PIECE.WProPawn, want: true },
   ]
-  test.each(tests)("promotionOf($piece) should be $want", ({piece, want}) => {
+  test.each(tests)("promotionOf($piece) should be $want", ({ piece, want }) => {
     expect(utils.promotionOf(piece)).toBe(want)
   })
 })
@@ -73,7 +73,7 @@ describe("toPos", () => {
     { square: SQUARE.SQ_23, want: [FILE.F2, RANK.R3] },
     { square: SQUARE.SQ_47, want: [FILE.F4, RANK.R7] },
   ]
-  test.each(tests)("toPos($square) should be $want", ({square, want}) => {
+  test.each(tests)("toPos($square) should be $want", ({ square, want }) => {
     expect(utils.toPos(square)).toEqual(want)
   })
 })
@@ -90,114 +90,114 @@ describe("toSquare", () => {
 })
 
 describe("toFile", () => {
-  const tests: Array<{square: Square; want: File}> = [
-    {square: SQUARE.SQ_11, want: FILE.F1},
-    {square: SQUARE.SQ_23, want: FILE.F2},
-    {square: SQUARE.SQ_47, want: FILE.F4},
+  const tests: Array<{ square: Square; want: File }> = [
+    { square: SQUARE.SQ_11, want: FILE.F1 },
+    { square: SQUARE.SQ_23, want: FILE.F2 },
+    { square: SQUARE.SQ_47, want: FILE.F4 },
   ]
-  test.each(tests)("toFile($square) should be $want", ({square, want}) => {
+  test.each(tests)("toFile($square) should be $want", ({ square, want }) => {
     expect(utils.toFile(square)).toBe(want)
   })
 })
 
 describe("toRank", () => {
-  const tests: Array<{square: Square; want: Rank}> = [
-    {square: SQUARE.SQ_11, want: RANK.R1},
-    {square: SQUARE.SQ_23, want: RANK.R3},
-    {square: SQUARE.SQ_47, want: RANK.R7},
+  const tests: Array<{ square: Square; want: Rank }> = [
+    { square: SQUARE.SQ_11, want: RANK.R1 },
+    { square: SQUARE.SQ_23, want: RANK.R3 },
+    { square: SQUARE.SQ_47, want: RANK.R7 },
   ]
-  test.each(tests)("toRank($square) should be $want", ({square, want}) => {
+  test.each(tests)("toRank($square) should be $want", ({ square, want }) => {
     expect(utils.toRank(square)).toBe(want)
   })
 })
 
 describe("filpFile", () => {
-  const tests: Array<{square: Square; want: Square}> = [
-    {square: SQUARE.SQ_11, want: SQUARE.SQ_91},
-    {square: SQUARE.SQ_23, want: SQUARE.SQ_83},
-    {square: SQUARE.SQ_47, want: SQUARE.SQ_67},
+  const tests: Array<{ square: Square; want: Square }> = [
+    { square: SQUARE.SQ_11, want: SQUARE.SQ_91 },
+    { square: SQUARE.SQ_23, want: SQUARE.SQ_83 },
+    { square: SQUARE.SQ_47, want: SQUARE.SQ_67 },
   ]
-  test.each(tests)("flipFile($square) should be $want", ({square, want}) => {
+  test.each(tests)("flipFile($square) should be $want", ({ square, want }) => {
     expect(utils.flipFile(square)).toBe(want)
   })
 })
 
 describe("filpRank", () => {
-  const tests: Array<{square: Square; want: Square}> = [
-    {square: SQUARE.SQ_11, want: SQUARE.SQ_19},
-    {square: SQUARE.SQ_23, want: SQUARE.SQ_27},
-    {square: SQUARE.SQ_47, want: SQUARE.SQ_43},
+  const tests: Array<{ square: Square; want: Square }> = [
+    { square: SQUARE.SQ_11, want: SQUARE.SQ_19 },
+    { square: SQUARE.SQ_23, want: SQUARE.SQ_27 },
+    { square: SQUARE.SQ_47, want: SQUARE.SQ_43 },
   ]
-  test.each(tests)("flipRank($square) should be $want", ({square, want}) => {
+  test.each(tests)("flipRank($square) should be $want", ({ square, want }) => {
     expect(utils.flipRank(square)).toBe(want)
   })
 })
 
 describe("hCount", () => {
-  const tests = [
-    {hand: 0b00_00_000_000_000_000_00000, role: ROLE.Pawn, want: 0},
-    {hand: 0b00_00_000_000_000_000_01010, role: ROLE.Pawn, want: 10},
-    {hand: 0b00_00_000_000_000_000_00010, role: ROLE.Lance, want: 0},
-    {hand: 0b00_00_000_000_000_001_00010, role: ROLE.Lance, want: 1},
-    {hand: 0b00_00_000_000_000_100_00010, role: ROLE.Lance, want: 4},
-    {hand: 0b00_00_000_000_001_100_00010, role: ROLE.Knight, want: 1},
-    {hand: 0b00_00_000_000_001_100_00010, role: ROLE.Bishop, want: 0},
-    {hand: 0b00_01_000_000_001_100_00010, role: ROLE.Bishop, want: 1},
-    {hand: 0b10_01_000_000_001_100_00010, role: ROLE.Rook, want: 2},
+  const tests: Array<{ hand: Hand; role: Role; want: number }> = [
+    { hand: 0b00_00_000_000_000_000_00000, role: ROLE.Pawn, want: 0 },
+    { hand: 0b00_00_000_000_000_000_01010, role: ROLE.Pawn, want: 10 },
+    { hand: 0b00_00_000_000_000_000_00010, role: ROLE.Lance, want: 0 },
+    { hand: 0b00_00_000_000_000_001_00010, role: ROLE.Lance, want: 1 },
+    { hand: 0b00_00_000_000_000_100_00010, role: ROLE.Lance, want: 4 },
+    { hand: 0b00_00_000_000_001_100_00010, role: ROLE.Knight, want: 1 },
+    { hand: 0b00_00_000_000_001_100_00010, role: ROLE.Bishop, want: 0 },
+    { hand: 0b00_01_000_000_001_100_00010, role: ROLE.Bishop, want: 1 },
+    { hand: 0b10_01_000_000_001_100_00010, role: ROLE.Rook, want: 2 },
   ]
-  test.each(tests)("", ({hand, role, want}) => {
+  test.each(tests)("", ({ hand, role, want }) => {
     expect(utils.hCount(hand, role)).toBe(want)
   })
 })
 
 describe("fromSq", () => {
-  const tests: Array<{move: Move; want: Square}> = [
-    {move: 0b00_0000001_0000000, want: SQUARE.SQ_12},
-    {move: 0b00_1000010_1000001, want: SQUARE.SQ_84},
+  const tests: Array<{ move: Move; want: Square }> = [
+    { move: 0b00_0000001_0000000, want: SQUARE.SQ_12 },
+    { move: 0b00_1000010_1000001, want: SQUARE.SQ_84 },
   ]
-  test.each(tests)("fromSq($move)", ({move, want}) => {
+  test.each(tests)("fromSq($move)", ({ move, want }) => {
     expect(utils.fromSq(move)).toBe(want)
   })
 })
 
 describe("toSq", () => {
-  const tests: Array<{move: Move; want: Square}> = [
-    {move: 0b00_0000001_0000000, want: SQUARE.SQ_11},
-    {move: 0b00_1000010_1000001, want: SQUARE.SQ_83},
+  const tests: Array<{ move: Move; want: Square }> = [
+    { move: 0b00_0000001_0000000, want: SQUARE.SQ_11 },
+    { move: 0b00_1000010_1000001, want: SQUARE.SQ_83 },
   ]
-  test.each(tests)("toSq($move)", ({move, want}) => {
+  test.each(tests)("toSq($move)", ({ move, want }) => {
     expect(utils.toSq(move)).toBe(want)
   })
 })
 
 describe("isPromotion", () => {
-  const tests: Array<{move: Move; want: boolean}> = [
-    {move: 0b00_0000001_0000000, want: false},
-    {move: 0b01_1000010_1000001, want: true},
+  const tests: Array<{ move: Move; want: boolean }> = [
+    { move: 0b00_0000001_0000000, want: false },
+    { move: 0b01_1000010_1000001, want: true },
   ]
-  test.each(tests)("isPromotion($move)", ({move, want}) => {
+  test.each(tests)("isPromotion($move)", ({ move, want }) => {
     expect(utils.isPromotion(move)).toBe(want)
   })
 })
 
 describe("isDrop", () => {
-  const tests: Array<{move: Move; want: boolean}> = [
-    {move: 0b00_0000001_0000000, want: false},
-    {move: 0b10_1000010_1000001, want: true},
+  const tests: Array<{ move: Move; want: boolean }> = [
+    { move: 0b00_0000001_0000000, want: false },
+    { move: 0b10_1000010_1000001, want: true },
   ]
-  test.each(tests)("isDrop($move)", ({move, want}) => {
+  test.each(tests)("isDrop($move)", ({ move, want }) => {
     expect(utils.isDrop(move)).toBe(want)
   })
 })
 
 describe("makeMove", () => {
-  const tests: Array<{from: Square; to: Square; promotion:boolean;drop:boolean;want: Move }> = [
-    {from: SQUARE.SQ_12, to: SQUARE.SQ_11, promotion: false, drop: false, want: 0b00_0000001_0000000},
-    {from: SQUARE.SQ_84, to: SQUARE.SQ_83, promotion: false, drop: false, want: 0b00_1000010_1000001},
-    {from: SQUARE.SQ_12, to: SQUARE.SQ_11, promotion: true, drop: false, want: 0b01_0000001_0000000},
-    {from: SQUARE.SQ_84, to: SQUARE.SQ_83, promotion: false, drop: true, want: 0b10_1000010_1000001},
+  const tests: Array<{ from: Square; to: Square; promotion: boolean; drop: boolean; want: Move }> = [
+    { from: SQUARE.SQ_12, to: SQUARE.SQ_11, promotion: false, drop: false, want: 0b00_0000001_0000000 },
+    { from: SQUARE.SQ_84, to: SQUARE.SQ_83, promotion: false, drop: false, want: 0b00_1000010_1000001 },
+    { from: SQUARE.SQ_12, to: SQUARE.SQ_11, promotion: true, drop: false, want: 0b01_0000001_0000000 },
+    { from: SQUARE.SQ_84, to: SQUARE.SQ_83, promotion: false, drop: true, want: 0b10_1000010_1000001 },
   ]
-  test.each(tests)("makeMove($move)", ({from, to, promotion, drop, want}) => {
+  test.each(tests)("makeMove($move)", ({ from, to, promotion, drop, want }) => {
     expect(utils.makeMove(from, to, promotion, drop)).toBe(want)
   })
 })
